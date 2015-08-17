f66a68594abfb5dd4fef67884215e7de00d0c5be
JairoCruz <azaveliz@gmail.com>
2015-08-17 11:43:46 -0600

    he agregado un ToolBar personalizado, y los pasos para hacerlo son los siguientes: Steps To set up a ToolBar 1. Use the Theme.AppCompat.Light.NoActionBar to prevent system action bar from showing (recordar que estamos trabajando con versiones anteriores a lolipor y para versiones lolipop tambien. esto lo digo por lo de "Theme.AppCompat"). 2. Define the app_bar.xml containing a ToolBar 3. Use <include> in your layout.xml file to add the ToolBar. 4. Instantiate the ToolBar using findViewById inside Activity 5. Call setSupportActionBar() and pass your ToolBar object inside 6. Customize the app:theme and app:popupTheme if necessary 7. Customize different properties of the ToolBar either using the toolbar object directly or getSupportActionBar()

--- app/src/main/java/com/example/tse/proyectmaterial/MainActivity.java
+++ app/src/main/java/com/example/tse/proyectmaterial/MainActivity.java
@@ -5,13 +5,20 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends ActionBarActivity{
 
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Para empezar a utilizar mi ToolBar personalizado
+        toolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
+
     }
 
     @Override
