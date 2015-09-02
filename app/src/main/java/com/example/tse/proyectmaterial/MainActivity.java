bdd2e5539537d82e15862fc2cb844101646201c1
JairoCruz <azaveliz@gmail.com>
2015-09-02 16:02:56 -0600

    se trabajo con el SlidingTab , se utilizo un unico layout para mostrar lo por medio de las tabs, estos asu vez le envian un argumento para mostrar en que posicion del tab estan. en el siguiente enlace estan las clases que se utilizaron para ayudar a utilizar las tabs https://github.com/google/iosched/blob/master/android/src/main/java/com/google/samples/apps/iosched/ui/widget/SlidingTabStrip.java

--- app/src/main/java/com/example/tse/proyectmaterial/MainActivity.java
+++ app/src/main/java/com/example/tse/proyectmaterial/MainActivity.java
@@ -4,15 +4,28 @@ package com.example.tse.proyectmaterial;
 import android.content.Intent;
 import android.os.Bundle;
 import com.example.tse.proyectmaterial.NavigationDrawerFragment;
+import com.example.tse.proyectmaterial.tabs.SlidingTabLayout;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity{
 
     private Toolbar toolbar;
+    private ViewPager mPager;
+    private SlidingTabLayout mTabs;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +40,13 @@ public class MainActivity extends ActionBarActivity{
         // Configuracion para poder utilizar el NavigationDrawer
         NavigationDrawerFragment drawerFragment = (NavigationDrawerFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
         drawerFragment.setUp(R.id.fragment_navigation_drawer,(DrawerLayout)findViewById(R.id.drawer_layout), toolbar);
+
+        // con esto utilizo las Tabs
+        mPager = (ViewPager) findViewById(R.id.pager);
+        mPager.setAdapter(new MyPagerAdapter(getSupportFragmentManager()));
+        mTabs = (SlidingTabLayout) findViewById(R.id.tabs);
+        mTabs.setViewPager(mPager);
+
 
     }
 
@@ -54,5 +74,56 @@ public class MainActivity extends ActionBarActivity{
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    // Esta clase esta relacionada con el uso de los tabs
+    class MyPagerAdapter extends FragmentPagerAdapter{
+
+        String[] tabs;
+
+        public MyPagerAdapter(FragmentManager fm) {
+            super(fm);
+            tabs = getResources().getStringArray(R.array.tabs);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            MyFragment myFragment = MyFragment.getInstance(position);
+            return myFragment;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return tabs[position];
+        }
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
+    }
+    // Esta clase tambien esta relacionada al uso de los Tabs
+    public static class MyFragment extends Fragment{
+        private TextView textView;
+        public static MyFragment getInstance(int position){
+            MyFragment myFragment = new MyFragment();
+            Bundle args = new Bundle();
+            // con esto le estoy pasando argumentos a mi fragment
+            args.putInt("position", position);
+            myFragment.setArguments(args);
+            return myFragment;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            View layout = inflater.inflate(R.layout.fragment_my,container,false);
+            textView = (TextView) layout.findViewById(R.id.position);
+            Bundle bundle = getArguments();
+            if(bundle != null){
+                textView.setText("The Page seleccionada es " + bundle.getInt("position"));
+            }
+            return layout;
+        }
     }
 }
