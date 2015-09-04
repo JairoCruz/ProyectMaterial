788180a9a5e2c7f324f18e9f01d87bd9d0510f71
JairoCruz <azaveliz@gmail.com>
2015-09-04 13:10:49 -0600

    he comenzado a trabajar con la libreria de Voley. en este primer commit he hecho una prueba de la libreria y he hecho una peticion sencilla utilizando un toas para mostrar informacion

--- app/src/main/java/com/example/tse/proyectmaterial/fragment/MyFragment.java
+++ app/src/main/java/com/example/tse/proyectmaterial/fragment/MyFragment.java
@@ -1,12 +1,20 @@
 package com.example.tse.proyectmaterial.fragment;
 
+import android.app.DownloadManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.tse.proyectmaterial.R;
 
 /**
@@ -34,6 +42,26 @@ public  class MyFragment extends Fragment {
         if(bundle != null){
             textView.setText("The Page seleccionada es " + bundle.getInt("position"));
         }
+
+
+        // Aca empezare a hacer uso de Volley Library
+        RequestQueue requestQueue = Volley.newRequestQueue(getActivity());
+        StringRequest request = new StringRequest(Request.Method.GET, "http://php.net/", new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Toast.makeText(getActivity(), "Response " + response, Toast.LENGTH_SHORT).show();
+            }
+        },new Response.ErrorListener(){
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getActivity(), "Error " + error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        requestQueue.add(request);
+
+
         return layout;
     }
 }
